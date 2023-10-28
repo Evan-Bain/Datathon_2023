3fe9a5a3088accc6e12340d927ff5446b2c24fbf
Evan Bain <evanjbain@gmail.com>
2023-10-28 14:31:57 -0500

    Basic project layout constructed. Documentation for MATLAB compatibility included.

---
+++ Documentation/Compatibility_MatLab/MatLab_Testing/MatLab_Functions/basic.m
@@ -0,0 +1,6 @@
+function sum_output = basic(inputArg1,inputArg2)
+    % print sum
+    %
+    % print the sum of the 2 arguements passed into function
+    sum_output = inputArg1 + inputArg2;
+end
---
+++ Documentation/Compatibility_MatLab/MatLab_Testing/MatLab_Functions/multiple_operations.m
@@ -0,0 +1,11 @@
+function [sum_out, diff_out, mult_out, div_out] = multiple_operations(first_num, second_num)
+    % return various mathmatical operations
+    %
+    % return sum, difference, multiplication, & division operations
+    % performed on arguements: first_num, second_num
+
+    sum_out = first_num + second_num;
+    diff_out = first_num - second_num;
+    mult_out = first_num * second_num;
+    div_out = first_num / second_num;
+end
